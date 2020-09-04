c3a922ac9babc2c476cf30113755e33264a4d660
Daniel.Yu <adam741963@gmail.com>
2020-09-04 12:46:09 -0700

    sql function

--- app.js
+++ app.js
@@ -12,14 +12,46 @@ const db = mysql.createConnection({
 
 db.connect(function(err) {
     if(err) throw err;
-   viewEmployee();
+
 });
+
+const mainOption = () => {
+    inquirer.prompt({
+        name: 'main',
+        type: 'list',
+        message: 'what you want',
+        choices: ['view', 'add','update' ,'delete']
+    }).then((answer) => {
+
+    });
+}
 
 const viewDepartment = () => {
     db.query(`SELECT department.name AS 'Department'
     FROM department`, function(err, res) {
         if(err) throw err;
         console.log(res);
+        db.end();
+    });
+}
+
+const addDepartment = () => {
+    db.query(`
+    INSERT INTO department (name)
+    VALUES (?)
+    `, 'department.name' ,function(err, res) {
+        if(err) throw err;
+        console.log('add department successed');
+        db.end();
+    });
+}
+
+const deleteDepartment = () => {
+    db.query(`
+    DELETE FROM department WHERE id = ?
+    `,'department.id' ,function(err, res) {
+        if(err) throw err;
+        console.log('deleted department');
         db.end();
     });
 }
@@ -34,6 +66,28 @@ const viewRole = () => {
     INNER JOIN role ON department.id = role.department_id`,function(err, res) {
         if(err) throw err;
         console.log(res);
+        db.end();
+    });
+}
+
+const addRole = () => {
+    db.query(`
+    INSERT INTO role (title, salary, department_id)
+    VALUES(?,?,?)
+    `,['role.title', 'role.salary', 'department_id'],
+    function(err, res) {
+        if(err) throw err;
+        console.log('added Role');
+        db.end();
+    });
+}
+
+const deleteRole = () => {
+    db.query(`
+    DELETE FROM role WHERE id = ?
+    `,'role.id' ,function(err, res) {
+        if(err) throw err;
+        console.log('deleted role');
         db.end();
     });
 }
@@ -56,5 +110,38 @@ const viewEmployee = () => {
         if(err) throw err;
         console.log(res);
         db.end();
+    });
+}
+
+const addEmployee = () =>{
+    db.query(`
+    INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?, ?, ? , ?)
+    `,['first', 'last','role', 'manager'] ,function (err, res) {
+        if(err) throw err;
+        console.log('add employee successed');
+        db.end();
+    });
+}
+
+const updateEmployee = () => {
+    db.query(`
+    UPDATE employee
+    SET role_id = ?
+    WHERE id = ?;
+    `, ['role_id','employee.id'], function(err, res) {
+        if(err) throw err;
+        console.log('updated employee');
+        db.end();
     })
+}
+
+const deleteEmployee = () => {
+    db.query(`
+    DELETE FROM employee WHERE id = ?
+    `, 'id', function(err, res) {
+        if(err) throw err;
+        if(res) console.log('delete employ successed');
+        db.end();
+    });
 }
